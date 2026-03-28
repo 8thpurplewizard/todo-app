f6665ebf50a65676157ab0c36224a289a2492057
8th Purple Wizard <the_d3ad@hotmail.com>
2026-03-28 17:22:58 +0200

    feat: initialize todo application with main components and styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div className="bg-gradient-to-tl from-fuchsia-800 to-teal-600 grid py-4 min-h-screen ">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
