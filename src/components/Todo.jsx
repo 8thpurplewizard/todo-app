bd5923734d84bb76e9231fe9c72833b55ff631c7
8th Purple Wizard <the_d3ad@hotmail.com>
2026-04-07 15:54:45 +0300

    feat: implement delete functionality for todo items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = () => {
     const inputRef = useRef();
     const [todoList, setTodoList] = useState([]);
 
-    const add = ()=>{
+    const addTodo = ()=>{
         const inputText = inputRef.current.value.trim();
 
         if(inputText === '') {
@@ -22,6 +22,12 @@ const Todo = () => {
         inputRef.current.value = '';
     }
 
+    const deleteTodo = (id) => {
+        setTodoList((prev)=>{
+            return prev.filter((todo) => todo.id !== id)
+        })
+    }
+
     return (
         <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col min-h-[550px] rounded-xl p-7">
             {/*title*/}
@@ -30,9 +36,9 @@ const Todo = () => {
                 <h1 className="text-3xl font-semibold">To-Do List</h1>
             </div>
             {/*input*/}
-            <div className="flex item-center my-7 bg-gray-200 rounded-full">   
+            <div className="flex item-center my-7 bg-gray-200 rounded-full">
                 <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-12 pl-6 pr-2 placeholder: text-slate-600" type="text" placeholder="Add your task"/>
-                <button onClick={add} className="border-none rounded-full bg-orange-600 w-12 h-12 text-white text-lg font-bold cursor-pointer">+</button>
+                <button onClick={addTodo} className="border-none rounded-full bg-orange-600 w-12 h-12 text-white text-lg font-bold cursor-pointer">+</button>
             </div>
             {/*list*/}
             <div>
@@ -40,10 +46,11 @@ const Todo = () => {
                     console.log('index:' + index + ' text:' + item.text);
                     
                     return <TodoItems 
-                    key={index}
+                    key={item.id}
                     id={item.id}
                     text={item.text}
                     isComplete={item.isComplete}
+                    deleteTodo={deleteTodo}
                     />
                 })}
             </div>
